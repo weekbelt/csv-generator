167e6ff011e127e257c91d20cd059b2d18eecb42
weekbelt <vfrvfr4207@gmail.com>
2021-02-02 21:16:30 +0900

    Initial commit

---
+++ src/main/java/com/posicube/robi/reception/ReceptionBackendDirectoryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.posicube.robi.reception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptionBackendDirectoryGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptionBackendDirectoryGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/posicube/robi/reception/ReceptionBackendDirectoryGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posicube.robi.reception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptionBackendDirectoryGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
