cbe9408470ff5f133bb042b852e29394d0bc5814
River <wnl668@naver.com>
2021-05-04 19:42:56 +0900

    fix: 보령 챗봇 질문답변 json 컨버터

--- src/main/java/com/posicube/robi/reception/domain/boryeong/dto/BoryeongDto.java
+++ src/main/java/com/posicube/robi/reception/domain/boryeong/dto/BoryeongDto.java
@@ -1,10 +1,18 @@
 package com.posicube.robi.reception.domain.boryeong.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class BoryeongDto {
 
     @JsonProperty("Question")
-    private String question;
+    private List<String> question;
+
+    @JsonProperty("Answer")
+    private String answer;
 
 }
--- src/main/java/com/posicube/robi/reception/domain/boryeong/service/BoryeongService.java
+++ src/main/java/com/posicube/robi/reception/domain/boryeong/service/BoryeongService.java
@@ -7,9 +7,12 @@ import com.posicube.robi.reception.util.CsvReaderUtil;
 import com.posicube.robi.reception.util.JsonUtil;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,20 +20,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class BoryeongService {
 
-    private final ObjectMapper objectMapper;
     private final CsvReaderUtil csvReaderUtil;
-    private final JsonUtil jsonUtil;
 
     public List<BoryeongDto> convertCsvToJson() throws CsvValidationException {
         List<BoryeongDto> list = new ArrayList<>();
         List<String[]> chatBotDF = csvReaderUtil.convertCsvResourceToDataFrame(new ClassPathResource("csv/br/chatBot/chatBot.csv"));
 
-        chatBotDF.forEach(chatBot -> {
-            log.info(chatBot.toString());
-        });
-
-
-
+        for (String[] st : chatBotDF) {
+            BoryeongDto dto = new BoryeongDto();
+            log.info("---------------");
+            log.info(st[5]);
+            dto.setQuestion(new ArrayList<>());
+            dto.getQuestion().add(st[5]);
+            StringTokenizer stringTokenizer = new StringTokenizer(st[6], System.lineSeparator());
+            while(stringTokenizer.hasMoreTokens()) {
+                String question = stringTokenizer.nextToken();
+                log.info(question);
+                dto.getQuestion().add(question);
+            }
+            log.info("+++++++++++++++++");
+            log.info(st[7]);
+            dto.setAnswer(st[7]);
+            list.add(dto);
+        }
         return list;
     }
+
 }
