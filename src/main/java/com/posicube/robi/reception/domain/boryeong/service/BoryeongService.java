68a676de056b4c642f2e7ef058c6a6d3360e6906
River <wnl668@naver.com>
2021-05-06 14:54:55 +0900

    fix: 컬럼번호 자동 파싱

--- src/main/java/com/posicube/robi/reception/domain/boryeong/service/BoryeongService.java
+++ src/main/java/com/posicube/robi/reception/domain/boryeong/service/BoryeongService.java
@@ -26,21 +26,31 @@ public class BoryeongService {
         List<BoryeongDto> list = new ArrayList<>();
         List<String[]> chatBotDF = csvReaderUtil.convertCsvResourceToDataFrame(new ClassPathResource("csv/br/chatBot/chatBot.csv"));
 
-        for (String[] st : chatBotDF) {
+        int questionNo1 = 5;
+        int questionNo2 = 6;
+        int answerNo = 7;
+
+        for (int i=0 ; i < chatBotDF.get(0).length ; i++) {
+            log.info(chatBotDF.get(0)[i]);
+            if ("대표질문".equals(chatBotDF.get(0)[i])) questionNo1 = i;
+            if ("유사질문".equals(chatBotDF.get(0)[i])) questionNo2 = i;
+            if ("답변".equals(chatBotDF.get(0)[i])) answerNo = i;
+        }
+        for (String[] row : chatBotDF) {
             BoryeongDto dto = new BoryeongDto();
             log.info("---------------");
-            log.info(st[5]);
+            log.info(row[questionNo1]);
             dto.setQuestion(new ArrayList<>());
-            dto.getQuestion().add(st[5]);
-            StringTokenizer stringTokenizer = new StringTokenizer(st[6], System.lineSeparator());
+            dto.getQuestion().add(row[questionNo1]);
+            StringTokenizer stringTokenizer = new StringTokenizer(row[questionNo2], System.lineSeparator());
             while(stringTokenizer.hasMoreTokens()) {
                 String question = stringTokenizer.nextToken();
                 log.info(question);
                 dto.getQuestion().add(question);
             }
             log.info("+++++++++++++++++");
-            log.info(st[7]);
-            dto.setAnswer(st[7]);
+            log.info(row[answerNo]);
+            dto.setAnswer(row[answerNo]);
             list.add(dto);
         }
         return list;
